b9f2da82b6a09063dab114109c3b73b65b5013d6
John Lang <55468621+Langutang@users.noreply.github.com>
2020-01-24 11:00:00 -0500

    Updated Admin

--- survey/appsurvey/admin.py
+++ survey/appsurvey/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from appsurvey.models import Survey
+from import_export.admin import ImportExportModelAdmin
+from import_export import  resources
 
+class SurveyResource(resources.ModelResource):
+
+    class Meta:
+        model = Survey
+        exclude = ('date')
+
+class SurveyAdmin(ImportExportModelAdmin):
+    resource_class = SurveyResource
 
 # Register your models here.
-admin.site.register(Survey)
+admin.site.register(Survey, SurveyAdmin)
